820e5cefd56ed6385bc206e3b7ae43b07300f80d
R-Anurag <146112209+R-Anurag@users.noreply.github.com>
2023-12-14 22:31:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -58,50 +58,56 @@ class PongGame(Widget):
     
 
     def serve_ball(self, *args):
-        self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(180-30, 180+30))
+        try:
+            self.ball.center = self.center
+            self.ball.velocity = Vector(4, 0).rotate(randint(180-30, 180+30))
+        except:
+            pass
+        
 
     def update(self, dt):
         
-        self.ball.move()
+        try:
+            self.ball.move()
+            # Bounce off paddles
+            self.player1_bounce()
+            self.player2_bounce()
 
-        # Bounce off paddles
-        self.player1_bounce()
-        self.player2_bounce()
+            # Bounce off top and bottom
+            if (self.ball.y < 0) or (self.ball.top > self.height):
+                self.ball.velocity_y *= -1
 
-        # Bounce off top and bottom
-        if (self.ball.y < 0) or (self.ball.top > self.height):
-            self.ball.velocity_y *= -1
+            # Score points
+            if self.ball.x < self.x:
+                self.miss_sound.play()
+                self.player2.score += 1
+                self.update_score_labels()
+                self.anim.start(self.player2_score_label)
+                if self.player2.score >= 10:
+                    self.serve_ball()
+                    Clock.unschedule(self.scheduledObject)
+                    self.show_winner("Computer")
+                else:
+                    # Clock.schedule_once( self.serve_ball, 2)
+                    self.serve_ball()
 
-        # Score points
-        if self.ball.x < self.x:
-            self.miss_sound.play()
-            self.player2.score += 1
-            self.update_score_labels()
-            self.anim.start(self.player2_score_label)
-            if self.player2.score >= 10:
-                self.serve_ball()
-                Clock.unschedule(self.scheduledObject)
-                self.show_winner("Computer")
-            else:
-                # Clock.schedule_once( self.serve_ball, 2)
-                self.serve_ball()
+            if self.ball.x > self.width:
+                self.score_sound.play()
+                self.player1.score += 1
+                self.update_score_labels()
+                self.anim.start(self.player1_score_label)
+                if self.player1.score >= 10:
+                    self.serve_ball()
+                    Clock.unschedule(self.scheduledObject)
+                    self.show_winner("Player")
+                else:
+                    # Clock.schedule_once( self.serve_ball, 2)
+                    self.serve_ball()
 
-        if self.ball.x > self.width:
-            self.score_sound.play()
-            self.player1.score += 1
-            self.update_score_labels()
-            self.anim.start(self.player1_score_label)
-            if self.player1.score >= 10:
-                self.serve_ball()
-                Clock.unschedule(self.scheduledObject)
-                self.show_winner("Player")
-            else:
-                # Clock.schedule_once( self.serve_ball, 2)
-                self.serve_ball()
-
-        # Computer-controlled paddle movement
-        self.move_computer_paddle()
+            # Computer-controlled paddle movement
+            self.move_computer_paddle()
+        except:
+            pass
     
     def move_computer_paddle(self):
         if self.ball.y < self.player2.center_y:
@@ -172,7 +178,7 @@ class PongApp(App):
         win_box.add_widget(Button(text="Reset", size_hint=(0.3, 0.15), pos_hint= {"center_x": 0.7, "center_y": 0.09}, on_release=App.get_running_app().game.replay_game))
         
         self.win_dialog = Popup(background="court2.jpg", title='Hurray! You Won', title_align="center",
-            title_color=(1,1,1,1), title_font="FlatlineSerif-Bold.otf", title_size=25, content=win_box,
+            title_color=(1,1,1,1), title_font="FlatlineSerif-Bold.ttf", title_size=25, content=win_box,
             size_hint=(None, None), size=(400, 400), auto_dismiss=False)
         
         lose_box = FloatLayout()
@@ -182,7 +188,7 @@ class PongApp(App):
         lose_box.add_widget(Button(text="Reset", size_hint=(0.3, 0.15), pos_hint= {"center_x": 0.7, "center_y": 0.09}, on_release=App.get_running_app().game.replay_game))
 
         self.lose_dialog = Popup(background_color=(0,0,0,0.7), title='Better Luck Next Time!', title_align="center",
-            title_color=(1,1,1,1), title_font="FlatlineSerif-Bold.otf", title_size=25, content=lose_box,
+            title_color=(1,1,1,1), title_font="FlatlineSerif-Bold.ttf", title_size=25, content=lose_box,
             size_hint=(None, None), size=(400, 400), auto_dismiss=False, separator_color="green")
 
     
